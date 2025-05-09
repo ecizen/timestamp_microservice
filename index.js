55172a5157ffc8f64098fda9d4806a36afb7706d
hardiektatendra <hardiektatendta@gmail.com>
2025-05-09 10:50:33 +0700

    first

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+const timestampRoute = require('./routes/timestamp')
+
+app.use('/api/timestamp', timestampRoute);
+
+app.listen(PORT, () => {
+  console.log(`Timestamp Microservice is running on port ${PORT}`);
+});
---
+++ routes/timestamp.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const date = new Date();
+  res.json({
+    unix: date.getTime(),
+    iso: date.toISOString(),
+    local: date.toLocaleString(),
+  });
+});
+
+router.get('/:date_string', (req, res) => {
+  let date = new Date(req.params.date_string);
+
+  if (date.toString() === "Invalid Date") {
+    const timestamp = parseInt(req.params.date_string);
+    if (!isNaN(timestamp)) {
+      date = new Date(timestamp);
+    }
+  }
+
+  if (date.toString() === "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({
+      unix: date.getTime(),
+      iso: date.toISOString(),
+      local: date.toLocaleString(),
+    });
+  }
+});
+
+module.exports = router;
